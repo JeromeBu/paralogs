e50b3f6edb4a57b4304ef75c104bf9446039af1a
Jérôme Burkard <burkardjerome@gmail.com>
2020-06-12 18:18:22 +0200

    add jest global config for back-auth

--- apps/back-auth/jest.config.js
+++ apps/back-auth/jest.config.js
@@ -2,4 +2,5 @@ module.exports = {
   name: "back-auth",
   preset: "../../jest.config.js",
   coverageDirectory: "../../coverage/apps/back-auth",
+  globalSetup: "./jestSetup.ts",
 };
---
+++ apps/back-auth/jestSetup.ts
@@ -0,0 +1,5 @@
+const setup = () => {
+  process.env.NODE_ENV = "test";
+  console.log("Starting test: NODE_ENV", process.env.NODE_ENV);
+};
+export default setup;
