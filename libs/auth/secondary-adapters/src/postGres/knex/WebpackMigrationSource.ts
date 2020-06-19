b786cdbebd2ac232d21eb6a912d0a2348dc1d323
Jérôme Burkard <burkardjerome@gmail.com>
2020-06-19 18:18:08 +0200

    reduce levels of folder nesting in auth secondary adapetrs

---
+++ libs/auth/secondary-adapters/src/postGres/knex/WebpackMigrationSource.ts
@@ -0,0 +1,17 @@
+export class WebpackMigrationSource {
+  constructor(migrationContext) {
+    this.migrationContext = migrationContext;
+  }
+
+  getMigrations() {
+    return Promise.resolve(this.migrationContext.keys().sort());
+  }
+
+  getMigrationName(migration) {
+    return migration;
+  }
+
+  getMigration(migration) {
+    return this.migrationContext(migration);
+  }
+}
